3193852a9fc6f1615df4d501a2a075470b253e71
Web3Domain <thachlan@outlook.com>
2022-08-30 11:06:31 +0700

    Add files via upload

--- test.js
+++ test.js
@@ -1,35 +1,5 @@
-# Syscoin Name Service
-
-Nodejs SDK
-
-Npm: https://www.npmjs.com/package/sysdomainjs
-
-Github: https://github.com/SyscoinNameService/sysdomainjs
-
-Before installing the package you need to check and be sure to install the packages below:
-
-```
-npm install web3 
-```
-
-Install Package
-
-```
-npm install sysdomainjs
-```
-
-Call 
-```
-const domainjs = require('sysdomainjs');
-```
-
-Set config
-
-contractAddress: "0xA1019535E6b364523949EaF45F4B17521c1cb074"
-
-https://explorer.syscoin.org/token/0xA1019535E6b364523949EaF45F4B17521c1cb074
-
-```
+const domainjs = require('metisdomainjs');
+// set config
 const config = 
 {
 	testnet:{
@@ -42,12 +12,12 @@ const config =
 	},
 	defaultNetwork: "mainnet"
 }
-```
 
-Install
+call();
 
-```
-   // install
+async function call(){
+	
+	// install
 	const sdk = domainjs.SDK(config);
 
 	// change your domains
@@ -64,7 +34,7 @@ Install
 	// get total domains
 	const balance = await sdk.balanceOf(_address);
 
-	console.log(balance);
+	console.log(balance.toString());
 
 	// get a domain default from a user's address, requiring the user to set the default domain name initially.
 	const domain = await sdk.getDomain(_address);
@@ -89,12 +59,6 @@ Install
 	//namehash is a recursive process that can generate a unique hash for any valid domain name.
 	const hashname = await sdk.hashname(_domain);
 
-	console.log(hashname);
-```
-
-Pls update test.js for specific instructions
-
-Thanks!
-
-
+	console.log(hashname.toString());
+}
 
